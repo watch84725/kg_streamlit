d5f963f60df2bd7adfbd3d93db1ff65d77736f7d
watch84725 <32084958+watch84725@users.noreply.github.com>
2024-08-20 10:44:19 +0800

    Add files via upload

--- kg_app.py
+++ kg_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-# 设置页面的基本样式
+# 設定頁面的基本樣式
 st.markdown("""
     <style>
     body {
@@ -14,9 +14,9 @@ st.markdown("""
     }
     .main {
         background-color: #ffffff;
-        padding: 10px;  /* 减少内边距，默认是20px */
+        padding: 10px;  /* 減少內邊距，默認是20px */
         border-radius: 8px;
-        max-width: 100%; /* 确保主容器最大宽度 */
+        max-width: 100%; /* 確保主容器最大寬度 */
     }
     h1, h2, h3 {
         color: #333;
@@ -38,10 +38,10 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# 显示大标题并置中
+# 顯示大標題並置中
 st.markdown("<h1 style='text-align: center; margin-bottom: 10px;'>第一屆康健增重大賽🍰</h1>", unsafe_allow_html=True)
 
-# 在右上角添加超链接，并进行美化
+# 在右上角添加超連結，並進行美化
 st.markdown(
     """
     <div style="text-align: right; padding: 20px;">
@@ -54,7 +54,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# 从 Streamlit Secrets 中获取 Google Sheets API 凭证
+# 從 Streamlit Secrets 中獲取 Google Sheets API 憑證
 creds_dict = st.secrets["GCP_SERVICE_ACCOUNT"]
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
@@ -70,31 +70,29 @@ data = sheet.get_all_records()
 # 將數據轉換為 DataFrame
 df = pd.DataFrame(data)
 
-# 将所有的日期列转换为数值类型，避免字符串类型影响计算
+# 將所有的日期列轉換為數值類型，避免字串類型影響計算
 date_columns = df.columns[1:]  # 除了 "Name" 列之外的所有日期列
 df[date_columns] = df[date_columns].apply(pd.to_numeric, errors='coerce')
 
-# 原始数据的处理
-# 将数据从宽表转换为长表（key-value 格式）
+# 將數據從寬表轉換為長表（key-value 格式）
 kg_longer_df = df.melt(id_vars=["Name"], var_name="Date", value_name="Weight")
 kg_longer_df = kg_longer_df[kg_longer_df['Date'] != '基準體重']
 
-# 清理数据：去除空字符串，确保所有列的数据类型正确
+# 清理數據：去除空字串，確保所有列的數據類型正確
 kg_longer_df['Date'] = pd.to_datetime(kg_longer_df['Date'], format="%m/%d", errors='coerce')
 kg_longer_df['Weight'] = pd.to_numeric(kg_longer_df['Weight'], errors='coerce')
 
-# 删除因转换错误产生的 NaN 值
+# 刪除因轉換錯誤產生的 NaN 值
 kg_longer_df = kg_longer_df.dropna(subset=['Date', 'Weight'])
 
-# 过滤出 Grace 和 Steven 的数据
+# 針對特定選手進行數據過濾
 grace_df = kg_longer_df[kg_longer_df['Name'] == 'Grace']
 steven_df = kg_longer_df[kg_longer_df['Name'] == 'Steven']
 perry_df = kg_longer_df[kg_longer_df['Name'] == 'Perry']
 alan_df = kg_longer_df[kg_longer_df['Name'] == 'Alan']
 
-
-# 选手一 Steven
-col1, col2 = st.columns([2, 5], gap="large")  # 适当调整 gap 以平衡左右间距
+# 顯示選手一 Steven 的數據圖表
+col1, col2 = st.columns([2, 5], gap="large")
 with col1:
     st.markdown("""
         <style>
@@ -123,7 +121,7 @@ with col2:
     ax1.set_ylabel('Weight (kg)')
     ax1.set_title('Steven Weight')
     
-    # 正确设置 y 轴的范围和刻度
+    # 正確設置 y 軸的範圍和刻度
     ax1.set_ylim(51.2, 54.4)
     ax1.set_yticks([51.3, 51.8, 52.3, 52.8, 53.3, 53.8, 54.3])
 
@@ -131,9 +129,8 @@ with col2:
     fig1.autofmt_xdate()
     st.pyplot(fig1)
 
-
-# 选手二 Grace
-col1, col2 = st.columns([2, 5], gap="large")  # 适当调整 gap 以平衡左右间距
+# 顯示選手二 Grace 的數據圖表
+col1, col2 = st.columns([2, 5], gap="large")
 with col1:
     st.markdown("""
         <style>
@@ -162,7 +159,7 @@ with col2:
     ax2.set_ylabel('Weight (kg)')
     ax2.set_title('Grace Weight')
     
-    # 正确设置 y 轴的范围和刻度
+    # 正確設置 y 軸的範圍和刻度
     ax2.set_ylim(43.7, 46.9)
     ax2.set_yticks([43.8, 44.3, 44.8, 45.3, 45.8, 46.3, 46.8])
 
@@ -170,8 +167,8 @@ with col2:
     fig2.autofmt_xdate()
     st.pyplot(fig2)
 
-# 选手三 Perry
-col1, col2 = st.columns([2, 5], gap="large")  # 适当调整 gap 以平衡左右间距
+# 顯示選手三 Perry 的數據圖表
+col1, col2 = st.columns([2, 5], gap="large")
 with col1:
     st.markdown("""
         <style>
@@ -195,21 +192,21 @@ with col1:
     st.image("Perry.PNG", use_column_width=True)
 with col2:
     fig3, ax3 = plt.subplots(figsize=(8, 4))
-    ax3.plot(perry_df['Date'], perry_df['Weight'], color='#f8af23', label='Perry')
+    ax3.plot(perry_df['Date'], perry_df['Weight'], color='#a4586aff', label='Perry')
     ax3.set_xlabel('Date')
     ax3.set_ylabel('Weight (kg)')
     ax3.set_title('Perry Weight')
     
-    # 正确设置 y 轴的范围和刻度
-    ax3.set_ylim(43.7, 46.9)
-    ax3.set_yticks([43.8, 44.3, 44.8, 45.3, 45.8, 46.3, 46.8])
+    # 正確設置 y 軸的範圍和刻度
+    ax3.set_ylim(46.8, 50.5)
+    ax3.set_yticks([46.9, 47.4, 47.9, 48.4, 48.9, 49.4, 49.9])
 
     ax3.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
     fig3.autofmt_xdate()
     st.pyplot(fig3)
 
-# 見證人 Alan
-col1, col2 = st.columns([2, 5], gap="large")  # 适当调整 gap 以平衡左右间距
+# 顯示見證人 Alan 的數據圖表
+col1, col2 = st.columns([2, 5], gap="large")
 with col1:
     st.markdown("""
         <style>
@@ -238,7 +235,7 @@ with col2:
     ax4.set_ylabel('Weight (kg)')
     ax4.set_title('Alan Weight')
     
-    # 正确设置 y 轴的范围和刻度
+    # 正確設置 y 軸的範圍和刻度
     ax4.set_ylim(70.3, 72.7)
     ax4.set_yticks([70.6, 71.1, 71.6, 72.1, 72.6, 73.1, 72.6])
 
@@ -246,66 +243,88 @@ with col2:
     fig4.autofmt_xdate()
     st.pyplot(fig4)
 
-# 新的 DataFrame：与 07/31 比较的体重增减
-base_date = '07/31'  # 基准日期
-base_weights = df[base_date]  # 07/31 的体重
-
-# 创建一个新的 DataFrame，计算每一天与 07/31 相比的体重增减
+# 計算 Grace 和 Steven 與 07/31 的體重變化
+base_date = '07/31'  # 基準日期
+base_weights = df[base_date]  # 07/31 的體重
+# 創建一個新的 DataFrame，計算每一天與 07/31 相比的體重變化
 df_change = df.copy()
 for col in date_columns:
     df_change[col] = df[col] - base_weights
 
-# 将数据从宽表转换为长表（key-value 格式）
+# 將數據從寬表轉換為長表（key-value 格式）
 kg_change_longer_df = df_change.melt(id_vars=["Name"], var_name="Date", value_name="WeightChange")
 
-# 清理数据：将日期转换为 datetime 格式，去除无效数据
+# 清理數據：將日期轉換為 datetime 格式，去除無效數據
 kg_change_longer_df['Date'] = pd.to_datetime(kg_change_longer_df['Date'], format="%m/%d", errors='coerce')
 
-# 删除 NaN 值
+# 刪除 NaN 值
 kg_change_longer_df = kg_change_longer_df.dropna(subset=['Date', 'WeightChange'])
 
-# 只保留 Grace 和 Steven 的数据
+# 只保留 Grace 和 Steven 的數據
 kg_change_longer_df_filtered = kg_change_longer_df[kg_change_longer_df['Name'].isin(['Grace', 'Steven'])]
 
+# 計算 Perry 與 08/16 的體重變化
+Perry_base_date = '8/16'  # 基準日期
+Perry_base_weights = df[Perry_base_date]  # 08/16 的體重
+# 創建一個新的 DataFrame，計算每一天與 08/16 相比的體重變化
+Perry_df_change = df.copy()
+for col in date_columns:
+    Perry_df_change[col] = df[col] - Perry_base_weights
+# 將數據從寬表轉換為長表（key-value 格式）
+Perry_kg_change_longer_df = Perry_df_change.melt(id_vars=["Name"], var_name="Date", value_name="WeightChange")
+# 清理數據：將日期轉換為 datetime 格式，去除無效數據
+Perry_kg_change_longer_df['Date'] = pd.to_datetime(Perry_kg_change_longer_df['Date'], format="%m/%d", errors='coerce')
+# 刪除 NaN 值
+Perry_kg_change_longer_df = Perry_kg_change_longer_df.dropna(subset=['Date', 'WeightChange'])
+# 只保留 Perry 的數據
+Perry_kg_change_longer_df_filtered = Perry_kg_change_longer_df[Perry_kg_change_longer_df['Name'].isin(['Perry'])]
+
+# 使用 concat 將兩個 DataFrame 進行 union
+result = pd.concat([kg_change_longer_df_filtered, Perry_kg_change_longer_df_filtered])
+
+# 重新設置索引（可選），如果你不需要原索引值，可以用 ignore_index=True
+result.reset_index(drop=True, inplace=True)
+
 # 找到最新的日期
-latest_date = kg_change_longer_df_filtered['Date'].max()
+latest_date = result['Date'].max()
 
-# 过滤出最新日期的数据
-latest_data = kg_change_longer_df_filtered[kg_change_longer_df_filtered['Date'] == latest_date]
-
-# 判断谁增重更多
+# 過濾出最新日期的數據
+latest_data = result[result['Date'] == latest_date]
+# 判斷誰增重更多
 leading_participant = latest_data.loc[latest_data['WeightChange'].idxmax(), 'Name']
 
-# 设置全局字体大小
-plt.rcParams.update({'font.size': 14})  # 修改全局字体大小
+# 設置全局字體大小
+plt.rcParams.update({'font.size': 14})  # 修改全局字體大小
 
-# 插入数据表格
+# 插入數據表格
 st.dataframe(df)
 
-# 第二个图：体重变化折线图
+# 第二個圖：體重變化折線圖
 fig2, ax2 = plt.subplots(figsize=(12, 6))
 
 ax2.plot(kg_change_longer_df_filtered[kg_change_longer_df_filtered['Name'] == 'Steven']['Date'], 
-         kg_change_longer_df_filtered[kg_change_longer_df_filtered['Name'] == 'Steven']['WeightChange'], 
-         marker='o', color='#2b7fb8', label='Steven')
+         kg_change_longer_df_filtered[kg_change_longer_df_filtered['Name'] == 'Steven']['WeightChange'], color='#2b7fb8', label='Steven')
 
 ax2.plot(kg_change_longer_df_filtered[kg_change_longer_df_filtered['Name'] == 'Grace']['Date'], 
-         kg_change_longer_df_filtered[kg_change_longer_df_filtered['Name'] == 'Grace']['WeightChange'], 
-         marker='o', color='#f8af23', label='Grace')
+         kg_change_longer_df_filtered[kg_change_longer_df_filtered['Name'] == 'Grace']['WeightChange'], color='#f8af23', label='Grace')
+
+ax2.plot(Perry_kg_change_longer_df_filtered[Perry_kg_change_longer_df_filtered['Name'] == 'Perry']['Date'], 
+         Perry_kg_change_longer_df_filtered[Perry_kg_change_longer_df_filtered['Name'] == 'Perry']['WeightChange'], color='#a4586aff', label='Perry')
+
 
 ax2.set_xlabel('Date', fontsize=14)
 ax2.set_ylabel('Weight Change (kg)', fontsize=14)
 ax2.set_title('Weight Leaderboard', fontsize=16)
 ax2.legend(title='Name')
 
-# 设置日期格式为 MM/DD
+# 設置日期格式為 MM/DD
 ax2.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
 
-# 自动调整日期标签，以避免重叠
+# 自動調整日期標籤，以避免重疊
 fig2.autofmt_xdate()
 
-# 在 Streamlit 中显示新图表
+# 在 Streamlit 中顯示新圖表
 st.pyplot(fig2)
 
-# 显示当前领先者
+# 顯示當前領先者
 st.markdown(f"<h2 style='text-align: center; color: #2b7fb8;'>目前領先者 🥇{leading_participant}</h2>", unsafe_allow_html=True)
